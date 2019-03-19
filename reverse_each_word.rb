b799c72d17513ae70a84681aa9cd05acf98667c3
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-03-19 20:08:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,19 @@
 def reverse_each_word(sentence)
+  words = sentence.split
+  words.each do |x|
+    x.reverse!
+    end
+    words.join(" ")
+end
+
+def reverse_each_word(sentence)
+  words = sentence.split
+  words.collect do |x|
+    x.reverse!
+    end
+    words.join(" ")
+end
+
+def reverse_each_word(sentence)
   sentence.reverse.split.reverse.join(" ")
 end
